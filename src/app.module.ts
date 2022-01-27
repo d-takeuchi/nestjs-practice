b484dca8edb4b21559527499ccaf59d0a61e4c75
takeuchi <kuronk.kuro3@gmail.com>
2022-01-27 18:04:27 +0900

    商品処理のCRUD機能追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ItemsModule } from './items/items.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ItemsModule, TypeOrmModule.forRoot()],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

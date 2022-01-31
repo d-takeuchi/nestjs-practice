aeab112cb731b88f08af827971ff2dcb3ecbd959
takeuchi <kuronk.kuro3@gmail.com>
2022-01-31 23:35:05 +0900

    ロールベース認可処理の実装

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -18,6 +18,9 @@ import { ItemsService } from './items.service'
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 import { GetUser } from 'src/auth/decorator/get-user.decorator'
 import { User } from 'src/entities/user.entity'
+import { Role } from 'src/auth/decorator/role.decorator'
+import { UserStatus } from 'src/auth/user-status.enum'
+import { RolesGuard } from 'src/auth/guards/roles.guard'
 
 @Controller('items')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -35,7 +38,8 @@ export class ItemsController {
   }
 
   @Post()
-  @UseGuards(JwtAuthGuard)
+  @Role(UserStatus.PREMIUM)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   async create(
     @Body() createItemDto: CreateItemDto,
     @GetUser() user: User,
